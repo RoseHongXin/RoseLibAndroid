68ad340aae22171b28beec24377418fd2b2f03c2
RoseHongXin <rose_hong@foxmail.com>
2018-09-12 18:21:50 +0800

    手表项目修改

--- src/Kit/java/hx/kit/view/ViewKit.java
+++ src/Kit/java/hx/kit/view/ViewKit.java
@@ -3,10 +3,8 @@ package hx.kit.view;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
-import android.support.v4.math.MathUtils;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -19,43 +17,29 @@ import java.lang.reflect.Field;
 
 public class ViewKit {
 
-    public static void hideInputManager(@NonNull EditText ... _ets){
-        Context ctx = _ets[0].getContext();
-        InputMethodManager inputMgr = ((InputMethodManager)ctx.getSystemService(Context.INPUT_METHOD_SERVICE));
-        for (EditText _et : _ets) {
-            inputMgr.hideSoftInputFromWindow(_et.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-            _et.clearFocus();
-//            boolean active = inputMgr.isActive(_et);
-//            if (active) {
-//                inputMgr.hideSoftInputFromWindow(_et.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-//                _et.clearFocus();
-//                return;
-//            }
-        }
-    }
-
-    public static void hideInputMgr(@NonNull View... _vs){
+    public static void hideInputMgr(@NonNull View... _vs) {
         Context ctx = _vs[0].getContext();
-        InputMethodManager inputMgr = ((InputMethodManager)ctx.getSystemService(Context.INPUT_METHOD_SERVICE));
-        for(View _v : _vs) {
+        InputMethodManager inputMgr = ((InputMethodManager) ctx.getSystemService(Context.INPUT_METHOD_SERVICE));
+        if (inputMgr == null) return;
+        for (View _v : _vs) {
             inputMgr.hideSoftInputFromWindow(_v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
             _v.clearFocus();
         }
     }
 
-    public static void showInputMgr(@NonNull View _v){
+    public static void showInputMgr(@NonNull View _v) {
         _v.requestFocus();
+        InputMethodManager inputMgr = ((InputMethodManager) _v.getContext().getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE));
+        if (inputMgr == null) return;
         _v.postDelayed(() -> {
-            InputMethodManager inputMgr = ((InputMethodManager)_v.getContext().getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE));
 //        inputMgr.showSoftInputFromInputMethod(_v.getWindowToken(), InputMethodManager.SHOW_FORCED);
 //        inputMgr.showSoftInput(_v, InputMethodManager.SHOW_FORCED);
             inputMgr.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, InputMethodManager.HIDE_NOT_ALWAYS);
         }, 100);
-
     }
 
 
-    public static void tlTabFixWidth(TabLayout tabLayout){
+    public static void tlTabFixWidth(TabLayout tabLayout) {
         tabLayout.post(() -> {
             try {
                 LinearLayout mTabStrip = (LinearLayout) tabLayout.getChildAt(0);
@@ -74,7 +58,7 @@ public class ViewKit {
                     }
 
                     LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) tabView.getLayoutParams();
-                    params.width = width ;
+                    params.width = width;
 //                        params.leftMargin = dp10;
 //                        params.rightMargin = dp10;
                     tabView.setLayoutParams(params);
