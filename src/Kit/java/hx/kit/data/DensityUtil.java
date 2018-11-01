f8fa91f97dd1afb8a8c91bffaaed854ecdbc0c6f
RoseHongXin <rose_hong@foxmail.com>
2018-11-01 18:51:20 +0800

    RecyclerView 滑动吸顶效果做好 FDeviceList页面结构修改

---
+++ src/Kit/java/hx/kit/data/DensityUtil.java
@@ -0,0 +1,25 @@
+package hx.kit.data;
+
+import android.content.Context;
+
+/**
+ * Created by RoseHongXin on 2018/11/1 0001.
+ */
+public class DensityUtil {
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
