91f52dc2fc79a46e21c52d8a879e4893c465f07e
RoseHongXin <rose_hong@foxmail.com>
2018-11-02 16:24:29 +0800

    1.授权相关调整. 2.断电原因功能添加. 3.UI以及代码优化.

--- src/Widgets/_Dialog/java/hx/widget/dialog/DWaiting.java
+++ src/Widgets/_Dialog/java/hx/widget/dialog/DWaiting.java
@@ -15,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import hx.kit.log.Log4Android;
@@ -35,7 +36,8 @@ public class DWaiting extends DialogFragment{
 
     private String TAG = "DWaiting";
 
-    TextView _tv_hint;
+    private TextView _tv_hint;
+    private ProgressBar _pb_roomInfo;
     private boolean mCancelable;
     private boolean mToast = false;
     private String mHint;
@@ -77,12 +79,15 @@ public class DWaiting extends DialogFragment{
             return true;
         });
         _tv_hint = (TextView) view.findViewById(R.id._tv_hint);
+        _pb_roomInfo = (ProgressBar) view.findViewById(R.id._pb_roomInfo);
+
         if(!TextUtils.isEmpty(mHint)) {
             _tv_hint.setVisibility(View.VISIBLE);
             _tv_hint.setText(mHint);
         }
         setCancelable(mCancelable);
         if(mToast){
+            _pb_roomInfo.setVisibility(View.GONE);
             view.postDelayed(this::dismiss, 2000);
         }
     }
@@ -104,23 +109,23 @@ public class DWaiting extends DialogFragment{
         return _tv_hint != null && _tv_hint.getText() != null ? _tv_hint.getText().toString() : "";
     }
 
-    public static DWaiting create(Activity act){
+    public static DWaiting create(@NonNull Activity act){
         return create(act, null);
     }
-    public static DWaiting create(Activity act, String hint){
+    public static DWaiting create(@NonNull Activity act, String hint){
         return create(act, hint, true);
     }
 
-    public static DWaiting force(Activity act, String hint){
+    public static DWaiting force(@NonNull Activity act, String hint){
         return create(act, hint, false);
     }
-    public static DWaiting force(Activity act){
+    public static DWaiting force(@NonNull Activity act){
         return create(act, null, false);
     }
-    public static DWaiting create(Activity act, @StringRes int hint, boolean cancelable){
+    public static DWaiting create(@NonNull Activity act, @StringRes int hint, boolean cancelable){
         return create(act, act.getString(hint), cancelable);
     }
-    public static DWaiting create(Activity act, String hint, boolean cancelable){
+    public static DWaiting create(@NonNull Activity act, String hint, boolean cancelable){
         DWaiting dWaiting = new DWaiting();
         dWaiting.mHint = hint;
         dWaiting.mCancelable = cancelable;
@@ -128,33 +133,44 @@ public class DWaiting extends DialogFragment{
         return dWaiting;
     }
 
-    public static DWaiting show(Activity act){
+    public static DWaiting show(@NonNull Activity act){
         return _show(act, null, true);
     }
-    public static DWaiting show(Activity act, @StringRes int hint){
+    public static DWaiting show(@NonNull Activity act, @StringRes int hint){
         return show(act, act.getString(hint));
     }
-    public static DWaiting show(Activity act, String hint){
+    public static DWaiting show(@NonNull Activity act, String hint){
         return _show(act, hint, true);
     }
-    public static DWaiting showForce(Activity act){
+    public static DWaiting showForce(@NonNull Activity act){
         return _show(act, null, false);
     }
-    public static DWaiting showForce(Activity act, @StringRes int hint){
+    public static DWaiting showForce(@NonNull Activity act, @StringRes int hint){
         return show(act, act.getString(hint));
     }
-    public static DWaiting showForce(Activity act, String hint){
+    public static DWaiting showForce(@NonNull Activity act, String hint){
         return _show(act, hint, false);
     }
-    public static DWaiting showToast(Activity act, String hint){
+    public static DWaiting showToast(@NonNull Activity act, @StringRes int hint){
+        return showToast(act, act.getString(hint));
+    }
+    public static DWaiting showToast(@NonNull Activity act, String hint){
         DWaiting dWaiting = create(act, hint, true);
         dWaiting.mToast = true;
         dWaiting.TAG = act.getClass().getSimpleName();
         dWaiting.show();
         return dWaiting;
+
+//        if(act.isFinishing() || act.isDestroyed()) return;
+//        DialogHelper.dlgButtonCenter(
+//                new AlertDialog.Builder(act)
+//                        .setTitle(hint)
+//                        .setCancelable(false)
+//                        .setNeutralButton(R.string.HX_confirm, null))
+//                .show();
     }
 
-    private static DWaiting _show(Activity act, String hint, boolean cancelable){
+    private static DWaiting _show(@NonNull Activity act, String hint, boolean cancelable){
         DWaiting dWaiting = new DWaiting();
         dWaiting.mAct = act;
         dWaiting.mHint = hint;
