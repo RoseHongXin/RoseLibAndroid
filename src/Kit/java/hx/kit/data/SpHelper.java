6b64a84a1b78ea47689eec04c4c7c6f9fc394980
RoseHongXin <rose_hong@foxmail.com>
2019-05-09 16:52:29 +0800

    删除项目中无用代码和类(包括之前遗留下来的SDK库全删)

--- src/Kit/java/hx/kit/data/SpHelper.java
+++ src/Kit/java/hx/kit/data/SpHelper.java
@@ -3,55 +3,43 @@ package hx.kit.data;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import com.alibaba.fastjson.JSON;
-
 public class SpHelper {
 
-    static private SharedPreferences mSp;
+    static private SharedPreferenceStub mSpStub;
 
     public SpHelper(Context ctx){
         init(ctx);
     }
 
     public static SharedPreferences get(){
-        return mSp;
+        return mSpStub.sharedPref();
     }
 
     public static void init(Context ctx){
-        mSp = ctx.getSharedPreferences(ctx.getPackageName(), Context.MODE_PRIVATE);
+        mSpStub = new SharedPreferenceStub(ctx, ctx.getPackageName());
     }
 
     public static String get(String key){
-        if(mSp == null) return "";
-        return mSp.getString(key, "");
+        return mSpStub.get(key);
     }
     public static int getInt(String key){
-        if(mSp == null) return 0;
-        return mSp.getInt(key, 0);
+        return mSpStub.getInt(key);
     }
     public static boolean getBoolean(String key, boolean def){
-        if(mSp == null) return def;
-        return mSp.getBoolean(key, def);
+        return mSpStub.getBoolean(key, def);
     }
 
     public static void set(String key, int data){
-        if(mSp != null) mSp.edit().putInt(key, data).apply();
+        mSpStub.set(key, data);
     }
     public static void set(String key, String data){
-        if(mSp != null) mSp.edit().putString(key, data).apply();
     }
     public static <T> void set(String key, T t){
-        String data = JSONHelper.toJSONStr(t);
-        if(mSp != null) mSp.edit().putString(key, data).apply();
     }
     public static void set(String key, boolean b){
-        if(mSp != null) mSp.edit().putBoolean(key, b).apply();
     }
 
     public static void clear(String key) {
-        if (mSp != null && mSp.contains(key)) {
-            mSp.edit().putString(key, "").apply();
-        }
     }
 
 }
