8a58d68381afffed960e947dd4387162eadd18ef
RoseHongXin <rose_hong@foxmail.com>
2019-07-31 12:12:49 +0800

    DItemBuConfirm 添加AleartDialog构建方式

--- src/Widgets/_Dialog/java/hx/widget/dialog/DItemBUConfirm.java
+++ src/Widgets/_Dialog/java/hx/widget/dialog/DItemBUConfirm.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.cncoderx.wheelview.Wheel3DView;
@@ -23,6 +24,7 @@ import java.util.List;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 import hx.lib.R;
@@ -35,6 +37,7 @@ public class DItemBUConfirm extends DialogFragment{
 
     private TextView _tv_title;
     private Wheel3DView _whv_items;
+    private Button _bt_confirm;
     private Object mTitle = null;
     private Callback mCb;
     private CharSequence[] mTexts;
@@ -44,80 +47,139 @@ public class DItemBUConfirm extends DialogFragment{
     private Activity mAct;
     private TextView _tv_anchor;
     private TextFormatCallback mTextFormatCallback;
+    private Object mBtnText;
+    private boolean mShow4Select = false;
+    private int mSelectedIdx = -1;
 
     public static DItemBUConfirm obtain() {
         return new DItemBUConfirm();
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        if(mShow4Select){
+            String positiveText = DialogHelper.getText(mAct, mBtnText);
+            if(TextUtils.isEmpty(positiveText)) positiveText = getString(R.string.HX_confirm);
+            CharSequence[] texts = getTextItems();
+            int defIdx = getDefItemIndex(texts);
+            AlertDialog.Builder builder = new AlertDialog.Builder(mAct)
+                    .setNegativeButton(R.string.HX_cancel, null)
+                    .setPositiveButton(positiveText, (dialog, which) -> {
+                        if(mSelectedIdx != -1){
+                            String text = mTexts.length > mSelectedIdx && mTexts[mSelectedIdx] != null ? mTexts[mSelectedIdx].toString() : "";
+                            onConfirmClick(mSelectedIdx, text);
+                        }
+                        dialog.dismiss();
+                    })
+                    .setSingleChoiceItems(texts, defIdx, (dialog, which) -> {
+                        mSelectedIdx = which;
+                    });
+            String title = DialogHelper.getText(mAct, mTitle);
+            if(!TextUtils.isEmpty(title)) builder.setTitle(title);
+            AlertDialog dialog = builder.create();
+            DialogHelper.dlgBtnFlat(dialog, null);
+            return dialog;
+        }
+        return super.onCreateDialog(savedInstanceState);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Dialog dialog = getDialog();
-        DialogHelper.erasePadding(dialog, Gravity.BOTTOM);
-        Window window = dialog.getWindow();
-        if(window != null) {
+        Window window = dialog != null ? dialog.getWindow() : null;
+        if (window != null) {
+            DialogHelper.erasePadding(dialog, Gravity.BOTTOM);
             window.setWindowAnimations(R.style.dialog_bottom_up);
-            window.setBackgroundDrawable(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
             WindowManager.LayoutParams params = window.getAttributes();
             params.dimAmount = 0.2f;
             window.setAttributes(params);
         }
+        if(mShow4Select){
+            if(window != null) window.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.windowBackground)));
+            return null;
+        }
+        if(window != null) window.setBackgroundDrawable(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
         return inflater.inflate(R.layout.d_item_bu_confirm, container, true);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        if(mShow4Select){
+
+        }else {
+            _show(view);
+        }
+    }
+
+    private void _show(View view){
         _tv_title = view.findViewById(R.id._tv_title);
         _whv_items = (Wheel3DView) view.findViewById(R.id._whv_items);
+        _bt_confirm = view.findViewById(R.id._bt_confirm);
         if(mTexts == null){
             _whv_items.setEntries();
             return;
         }
-        if(mTextFormatCallback == null) {
-            _whv_items.setEntries(mTexts);
-        }else{
-            CharSequence[] formattedTexts = new CharSequence[mTexts.length];
-            for(int i = 0; i < mTexts.length; i++){
-                CharSequence text = mTextFormatCallback.onFormat(mTexts[i]);
-                formattedTexts[i] = text;
-            }
-            _whv_items.setEntries(formattedTexts);
-        }
-        int defIdx = -1;
-        if(mDefValue != null){
-            for(int i = 0; i < mTexts.length; i++){
-                if(TextUtils.equals(mTexts[i], mDefValue)){
-                    defIdx = i;
-                    break;
-                }
-            }
-        }
+        CharSequence[] texts = getTextItems();
+        _whv_items.setEntries(texts);
+        int defIdx = getDefItemIndex(texts);
         if(defIdx != -1){
             _whv_items.setCurrentIndex(defIdx);
         }
         int itemCount = mTexts.length;
         if(itemCount <= 3) _whv_items.setVisibleItems(itemCount + 1);
-        view.findViewById(R.id._bt_confirm).setOnClickListener(v -> {
+
+        String btnText = DialogHelper.getText(mAct, mBtnText);
+        if(!TextUtils.isEmpty(btnText)) _bt_confirm.setText(btnText);
+        _bt_confirm.setOnClickListener(v -> {
             int idx = _whv_items.getCurrentIndex();
             String text = _whv_items.getCurrentItem().toString();
-            if(_tv_anchor != null) {
-                _tv_anchor.setText(text);
-                _tv_anchor.setTag(idx);
-            }
-            if(mCb != null) {
-                if(mValues == null) mCb.onSelect(idx, text);
-                else mCb.onSelect(idx < mValues.length ? mValues[idx] : 0, text);
-            }
+            onConfirmClick(idx, text);
             dismiss();
         });
         if(mTitle != null){
             _tv_title.setVisibility(View.VISIBLE);
             if(mTitle instanceof String) _tv_title.setText((String)mTitle);
             else if(mTitle instanceof Integer) _tv_title.setText((Integer) mTitle);
-            else _tv_title.setVisibility(View.INVISIBLE);
+            else _tv_title.setVisibility(View.GONE);
         }
     }
+
+    private CharSequence[] getTextItems(){
+        if(mTextFormatCallback == null) {
+            return mTexts;
+        }else{
+            CharSequence[] formattedTexts = new CharSequence[mTexts.length];
+            for(int i = 0; i < mTexts.length; i++){
+                CharSequence text = mTextFormatCallback.onFormat(mTexts[i]);
+                formattedTexts[i] = text;
+            }
+            return formattedTexts;
+        }
+    }
+    private int getDefItemIndex(CharSequence[] texts){
+        if(mDefValue != null) {
+            for (int i = 0; i < texts.length; i++) {
+                if (TextUtils.equals(texts[i], mDefValue)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    private void onConfirmClick(int index, String text){
+        if(_tv_anchor != null) {
+            _tv_anchor.setText(text);
+            _tv_anchor.setTag(index);
+        }
+        if(mCb != null) {
+            if(mValues == null) mCb.onSelect(index, text);
+            else mCb.onSelect(index < mValues.length ? mValues[index] : 0, text);
+        }
+    }
+
 
     public DItemBUConfirm host(Activity act){
         mAct = act;
@@ -126,6 +188,10 @@ public class DItemBUConfirm extends DialogFragment{
 
     public DItemBUConfirm callback(Callback cb){
         mCb = cb;
+        return this;
+    }
+    public DItemBUConfirm btn(Object text){
+        mBtnText = text;
         return this;
     }
     public DItemBUConfirm title(Object title){
@@ -152,7 +218,7 @@ public class DItemBUConfirm extends DialogFragment{
     }
     public DItemBUConfirm icons(Context ctx, @DrawableRes int ... icons){
         List<Drawable> drawables = new ArrayList<>();
-        for(int i : icons) drawables.add(ctx.getDrawable(i));
+        for(int i : icons) drawables.add(ctx.getResources().getDrawable(i));
         return icons(drawables);
     }
     public DItemBUConfirm textFormat(TextFormatCallback callback){
@@ -194,6 +260,12 @@ public class DItemBUConfirm extends DialogFragment{
     }
 
     public DItemBUConfirm show(){
+        show(((AppCompatActivity)mAct).getSupportFragmentManager(), "DItemBUConfirm");
+        return this;
+    }
+
+    public DItemBUConfirm show4select(){
+        mShow4Select = true;
         show(((AppCompatActivity)mAct).getSupportFragmentManager(), "DItemBUConfirm");
         return this;
     }
--- src/Widgets/_Dialog/java/hx/widget/dialog/DialogHelper.java
+++ src/Widgets/_Dialog/java/hx/widget/dialog/DialogHelper.java
@@ -1,14 +1,16 @@
 package hx.widget.dialog;
 
 import android.app.Dialog;
-import androidx.appcompat.app.AlertDialog;
+import android.content.Context;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import androidx.appcompat.app.AlertDialog;
 import hx.lib.R;
 
 /**
@@ -65,6 +67,36 @@ public class DialogHelper {
         });
 
         return dialog;
+    }
+    public static void dlgBtnFlat(AlertDialog dialog, AlertDialog.OnShowListener onShowListener){
+        DisplayMetrics metrics = new DisplayMetrics();
+        Window window = dialog.getWindow();
+        if(window != null) {
+            window.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        }
+        dialog.setOnShowListener(dlg -> {
+            dialog.getButton(AlertDialog.BUTTON_NEUTRAL).setVisibility(View.GONE);
+            Button _bt_positive = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+            View _v_container = (View) _bt_positive.getParent();
+            int width = metrics.widthPixels / 2 - _v_container.getPaddingLeft() - _v_container.getPaddingRight();
+            LinearLayout.LayoutParams layoutParams  = (LinearLayout.LayoutParams) _bt_positive.getLayoutParams();
+            layoutParams.width = width;
+            layoutParams.gravity = Gravity.END|Gravity.CENTER_VERTICAL;
+            _bt_positive.setLayoutParams(layoutParams);
+            Button _bt_negative = dialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+            layoutParams  = (LinearLayout.LayoutParams) _bt_negative.getLayoutParams();
+            layoutParams.width = width;
+            layoutParams.gravity = Gravity.START|Gravity.CENTER_VERTICAL;
+            _bt_negative.setLayoutParams(layoutParams);
+            if(onShowListener != null) onShowListener.onShow(dlg);
+        });
+    }
+
+    static String getText(Context ctx, Object text){
+        if (text == null) return "";
+        else if(text instanceof String) return (String) text;
+        else if(text instanceof Integer) return ctx.getString((Integer) text);
+        else return "";
     }
 
 }
